c5f9cafd5ac3013b03133b7b3caaf6790a4431b6
MaurooCx <maury.22xd@gmail.com>
2022-12-14 23:36:30 -0600

    Seleccionar ubicacione

--- src/public/javascripts/googleMaps.js
+++ src/public/javascripts/googleMaps.js
@@ -1,44 +1,53 @@
-function initMap() {
-    const TL = { lat: 19.5059214, lng: -99.1466907 };
-    const SFe = { lat: 19.3611053, lng: -99.2757859};
-    const Fom = { lat: 19.4496778, lng: -99.1538179};
-    const Ant = { lat: 19.3138125, lng: -99.0784336};
+const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let labelIndex = -1;
 
+function initMap() {
+    const Centro = { lat: 19.3911668 , lng: -99.1466907 };
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 11.3,
-      center: { lat:19.3911668 , lng:   -99.1466907 },
+      center: Centro,
+    });
+    const tourStops = [
+      [{ lat: 19.5059214, lng: -99.1466907 }, "Plaza Torres Lindavista"],
+      [{ lat: 19.3611053, lng: -99.2757859}, "Centro Santa Fe"],
+      [{ lat: 19.4496778, lng: -99.1538179}, "Plaza Forum"],
+      [{ lat: 19.3138125, lng: -99.0784336}, "Plaza Las Antenas"],
+    ];
+
+    const infoWindow = new google.maps.InfoWindow();
+
+    tourStops.forEach(([position, title], i) => {
+      const marker = new google.maps.Marker({
+        position,
+        map,
+        title: `${i + 1}. ${title}`,
+        label: `${i + 1}`,
+        optimized: false,
+      });
+    marker.addListener("click", () => {
+      infoWindow.close();
+      infoWindow.setContent(marker.getTitle());
+      infoWindow.open(marker.getMap(), marker);
+     });
     });
     const map1 = new google.maps.Map(document.getElementById("map1"), {
       zoom: 11.3,
-      center: { lat:19.3911668 , lng:   -99.1466907 },
-    });
-    new google.maps.Marker({
-      position: TL,
-      map,
-      title: "Plaza Torres Lindavista",
-    });
-    new google.maps.Marker({
-      position: SFe,
-      map,
-      title: "Centro Santa Fe",
-    });
-    new google.maps.Marker({
-      position: Fom,
-      map,
-      title: "Plaza Forum",
-    });
-    new google.maps.Marker({
-      position: Ant,
-      map,
-      title: "Plaza Las Antenas",
+      center: Centro,
     });
 
+    google.maps.event.addListener(map1, "click", (event) => {
+      addMarker(event.latLng, map1);
+    });
+  }
+
+  function addMarker(location, map1) {
+    new google.maps.Marker({
+      position: location,
+      //label: labels[labelIndex++ % labels.length],
+      map:map1,
+    });
   }
   
-  const locations = [
-    { lat: 19.3138125, lng: -99.0784336 },
-  ];
-
   
   window.initMap = initMap;
   window.initMap = initMap;
