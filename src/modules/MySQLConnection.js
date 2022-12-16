076155ef663ee0152dc5749e49d444ac3e2d01fc
MaurooCx <maury.22xd@gmail.com>
2022-12-15 23:26:13 -0600

    anexos sur oriente

--- src/modules/MySQLConnection.js
+++ src/modules/MySQLConnection.js
@@ -6,7 +6,6 @@ var con = mysql.createConnection({
 	user: 		process.env.MYSQL_USER,
 	password:	process.env.MYSQL_PASSWORD,
 	database:	process.env.MYSQL_DATABASE,
-	charset:    "utf-8",
 });
 
 // loop if fails to connect
