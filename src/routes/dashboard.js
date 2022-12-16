b38ffb61cd2db98dd24ed8ddd62275e96ef44ace
Edu-Olv20 <edudav2000@gmail.com>
2022-12-15 19:58:28 -0600

    Metodo de Pago Visualizar

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -150,7 +150,17 @@ router.get('/envio/crearEnvio/createSize', (req,res,next) =>{
 router.get('/envio/crearEnvio/payment', (req,res,next) =>{
     // res.render("createSize") 
     if (req.session.user) {
-        res.render('payment' , { title: 'sendiit - panel', path: req.path, user: req.session.user });
+        db.getmetodosDePagos(req.session.user).then((results)=>{
+			debug('results', results);
+			if (results.length) {
+				res.render('payment' , { title: 'sendiit - panel', path: req.path, user: req.session.user, metodosDePagos:results});
+			}
+			else {
+				res.status(401).json({response:'ERROR', message:'Rutas completadas no encontradas'});
+			}
+		});
+        //res.render('payment' , { title: 'sendiit - panel', path: req.path, user: req.session.user, metodosDePagos: metodosDePagos });
+
     } else {
         res.redirect('/');
     }
