23ac470ca3ea402b1cefa97100cc99a9f8009a42
Gpjonat <jonatgp5@gmail.com>
2022-12-16 01:05:29 -0600

    mapas ya bien porfin alv
    Co-authored-by: DannyDelValle <DannyDelValle@users.noreply.github.com>

--- src/public/javascripts/googleMaps.js
+++ src/public/javascripts/googleMaps.js
@@ -1,33 +1,62 @@
+const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let labelIndex = 0;
+
 console.log(addres)
 var add =addres.map(e =>e.dir_lkr)
+var addnm =addres.map(e =>e.nm_lkr)
 console.log(add)
 var geocoder;
 var map;
 //var address = 'Av. Miguel Othón de Mendizábal Ote. 343, Nueva Industrial Vallejo, Gustavo A. Madero, 07700 Ciudad de México, CDMX' ;
+
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 11,
-    center: {lat:19.440744027382173, lng: -99.12751310305093}
+
+
+ var map = new google.maps.Map(document.getElementById('map'), {
+   zoom: 11,
+   center: {lat:19.440744027382173, lng: -99.12751310305093}
+ });
+ tam =add.length
+ geocoder = new google.maps.Geocoder();
+ for(var i = 0; i < tam; i++){
+   address = add[i]
+   namew = addnm[i]
+   codeAddress(geocoder, map,address,namew,i)
+
+ }
+
+
+ marker.addListener("click", () => {
+   console.log('me estoy ejecuntand')
+   infoWindow.close();
+   infoWindow.setContent(marker.getTitle());
+   infoWindow.open(marker.getMap(), marker);
   });
-  geocoder = new google.maps.Geocoder();
-  for(address of add){
-    codeAddress(geocoder, map,address)
-  }
-  
+ 
 }
 
-function codeAddress(geocoder, map,address) {
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === 'OK') {
-      var marker = new google.maps.Marker({
-        map: map,
-        position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
+function codeAddress(geocoder, map,address,namew,i) {
+  const infoWindow = new google.maps.InfoWindow();
+ geocoder.geocode({'address': address}, function(results, status) {
+   if (status === 'OK') {
+     const marker = new google.maps.Marker({
+       map: map,
+       position: results[0].geometry.location,
+       title: `${i + 1}.${namew}`,
+       label: `${i + 1}`,
+       optimized: false,
+     });
+     marker.addListener("click", () => {
+      infoWindow.close();
+      infoWindow.setContent(marker.getTitle());
+      infoWindow.open(marker.getMap(), marker);
+     });
+   } else {
+     alert('Geocode was not successful for the following reason: ' + status);
+   }
+ });
 }
-  
+
   
 window.initMap = initMap;
+
