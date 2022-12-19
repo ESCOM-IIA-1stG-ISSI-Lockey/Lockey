73d7c10ce7860faa8d55f08cf3e924c8985658b0
M1ndBlast <gustavopdzz0@gmail.com>
2022-12-18 18:20:28 -0600

    New modules on index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,152 +1,100 @@
-var debug = require('debug')('lockey:router:index');
-var express = require('express');
-var router = express.Router();
-var crypto = require('node:crypto');
-var db = require('../modules/MySQLConnection');
-var mailer = require('../modules/SendGmailV');
+const debug = require('debug')('lockey:router:index');
+const express = require('express');
+const router = express.Router();
+const crypto = require('node:crypto');
+const db = require('../modules/MySQLConnection');
+const Auth = require('../modules/Auth');
+const Validator = require('../modules/Validator');
+const mailer = require('../modules/SendGmailV');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	if (req.session.user) 
-		res.redirect('/panel');
-	else 
-		res.render('index', { title:'sendiit', path:req.path, user:req.session.user });
+router.route('/')
+.get(Auth.onlyGuests, (req, res, next) => {
+	res.render('index', { title: 'sendiit', path: req.path, user: req.session.user });
 });
 
-
 router.route('/identificacion')
-	.post((req, res, next) => {
-		debug(req.body);
-		let {email, password} = req.body;
+.post(Auth.onlyGuests, Validator.signin,
+	(req, res, next) => {
+		let { email, password } = req.body;
 		password = crypto.createHash('sha256').update(password).digest('hex');
 
 		db.checkCredentials(email, password).then((results) => {
-			debug('results', results);
 			if (results.length) {
-				if(results[0].act_usr == 'ENABLED'){
-					req.session.user = {
-						id: results[0].id_usr,
-						name: results[0].nm_usr,
-						email: results[0].em_usr,
-						type: results[0].type_usr,
-						isActive: results[0].act_usr
-					};
-					debug('/identificacion session.user:', req.session.user);
-					
-					res.status(200).json({
-						response: 'OK',
-						message: 'Usuario autenticado',
-						redirect: '/'
-					});
-				}
-				else{
+				if (results[0].act_usr == 'ENABLED')
+					Auth.createSession(req, results[0]);
+				else {
 					req.session.tmpemail = email;
 					res.status(200).json({
 						response: 'OK',
 						message: 'Usuario creado correctamente',
-						modal:{
-							old:'#signinModal',
+						modal: {
+							old: '#signinModal',
 							new: '#mailverificationModal',
 						},
-					
 					});
-					console.log('fernando')
 				}
 
-			} else {
-				res.status(401).json({response:'ERROR', message:'Correo o contraseña incorrectos'});
 			}
+			else
+				throw new Error('Correo o contraseña incorrectos');
+
 		}).catch((err) => {
-			res.status(402).json({response:'ERROR', message:err});
+			res.status(400).json({ response: 'ERROR', message: err.message });
 		});
 	});
-
 router.route('/registro')
-	.post((req, res, next) => {
-		let {name, email, tel, password} = req.body;
+.post(Auth.onlyGuests, Validator.signup,
+	(req, res, next) => {
+		let { name, email, tel, password } = req.body,
+			token = Math.floor(Math.random() * 1000000); // Random 6-digit number
+
 		password = crypto.createHash('sha256').update(password).digest('hex');
-		
-		let token = generateToken();
-		// Genera token para guardar en Base de datos
-		
+
 		db.createUser(name, email, tel, password, token, db.ROLES.CLIENT).then((results) => {
-			if (results.affectedRows > 0) {
-				db.getUserById(results.insertId).then((results) => {
-					
-					if (results.length > 0) {
-
-						
-						mailer.mailVerification(email, token);
-
-						req.session.tmpemail = email;
-
-						res.status(200).json({
-							response: 'OK',
-							message: 'Usuario creado correctamente',
-							modal:{
-								new: '#mailverificationModal',
-								old: '#signupModal'
-							},
-						});
-					} else {
-						res.status(401).json({response:'ERROR', message:'Usuario no encontrado tras registro'});
-					}
-				}).catch((err) => {
-					debug(err);
-					res.status(402).json({response:'ERROR', message:err});
-				});
-			} else {
-				res.status(401).json({response:'ERROR', message:'No se pudo crear el usuario'});
-			}
-		}).catch((err) => {
+			if (results.affectedRows > 0)
+				return db.getUserById(results.insertId)	
+			else throw new Error('No se pudo crear el usuario');
+		})
+		.then((results) => {
+			if (results.length > 0)
+				return mailer.mailVerification(email, token);
+			else throw new Error('Usuario no encontrado tras registro');		
+		})
+		.then(() => {
+			req.session.tmpemail = email;
+			res.status(200).json({
+				response: 'OK',
+				message: 'Usuario creado correctamente',
+				modal: {
+					new: '#mailverificationModal',
+					old: '#signupModal'
+				},
+			});
+		})
+		.catch((err) => {
 			debug(err);
-			res.status(402).json({response:'ERROR', message:err});
+			res.status(400).json({ response: 'ERROR', message: err });
 		});
 	});
 
-	function generateToken() {
-		let num = 0;
-		let random = Math.random();
-		random = random * (899999) + 100000;
-		random = Math.trunc(random);
-		num = random;
-		console.log(num);
-		
-		return num;
-	}
-
-	router.route('/verificador')
-	.post((req, res, next) => {
+router.route('/verificador')
+.post(Auth.onlyGuests, Validator.token,
+	(req, res, next) => {
 		debug(req.body);
-		
-		let {NUM1, NUM2, NUM3, NUM4,NUM5,NUM6} = req.body;
-		const fullnumber = NUM1 + NUM2 + NUM3 + NUM4+ NUM5 + NUM6;
-		let VerifyNumber = Number(fullnumber);
-		let email = req.session.tmpemail;
+
+		let { NUM1, NUM2, NUM3, NUM4, NUM5, NUM6 } = req.body,
+			VerifyNumber = NUM1 + NUM2 + NUM3 + NUM4 + NUM5 + NUM6,
+			email = req.session.tmpemail;
 
 		db.verifycode(email, VerifyNumber).then((results) => {
-			if (results.length) {
-				req.session.user = {
-					id: results[0].id_usr,
-					name: results[0].nm_usr,
-					email: results[0].em_usr,
-					type: results[0].type_usr
-				};
-				
-				res.status(200).json({
-					response: 'OK',
-					message: 'Usuario autenticado',
-					redirect: '/'
-				});
-			} else {
-				res.status(401).json({response:'ERROR', message:'Código Inválido'});
-			}
+			if (results.length)
+				Auth.createSession(req, results[0]);
+			else
+				throw new Error('Código Inválido');
 		}).catch((err) => {
 			debug(err);
-			res.status(402).json({response:'ERROR', message:err});
+			res.status(400).json({ response: 'ERROR', message: err });
 		});
-
-
 	});
 
 
