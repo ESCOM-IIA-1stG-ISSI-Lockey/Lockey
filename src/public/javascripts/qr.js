75253ea4c2fb9191a401678e47e546a7044e4ee4
AliLM <ali.lm082000@gmail.com>
2022-12-31 00:46:10 -0600

    QR generation

--- src/public/javascripts/qr.js
+++ src/public/javascripts/qr.js
@@ -1,6 +1,27 @@
-const canvas = document.getElementById('qr-canvas')
+const opts = {
+	errorCorrectionLevel: 'H',
+	type: 'image/webp',
+	quality: 1.0,
+	margin: 1,
+	scale: 10,
+	small: true,
+	color: {
+		dark:"#212e46",
+		light:"ffffff"
+	}	
+};
 
-QRCode.toCanvas(canvas, 'sample text', function (error) {
-  if (error) console.error(error)
-  console.log('success!');
-})
+// Select all the elements with the class name 'qr-canvas'
+let qrImgs = document.querySelectorAll('.qr-img');
+
+// Loop through the elements
+qrImgs.forEach(function (img) {
+	  // Get the text from the data attribute
+	  let text = img.getAttribute('data-text');
+	  // Generate the QR code
+	  QRCode.toDataURL(text, opts, function (error, url) {
+		if (error) console.error(error)
+		console.log(url)
+		img.src = url;
+	  })
+});
